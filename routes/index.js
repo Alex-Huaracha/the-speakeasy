93fc859cecacda61e2b45deff61edfd4750b7a4c
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-12 14:31:42 -0500

    feat(routes): update main router to use message controller for message-related routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 
 import * as userController from '../controllers/userController.js';
-
+import * as messageController from '../controllers/messageController.js';
 const router = express.Router();
 
 // Principal Route
-router.get('/', (req, res) => res.render('index', { title: 'Home' }));
-
+router.get('/', messageController.message_list_get);
 // Sign Up Routes
 router.get('/sign-up', userController.user_signup_get);
 router.post('/sign-up', userController.user_signup_post);
@@ -21,5 +20,12 @@ router.get('/log-out', userController.user_logout_get);
 // Membership Join Group
 router.get('/join-club', userController.join_club_get);
 router.post('/join-club', userController.join_club_post);
+
+// Create message
+router.get('/create-message', messageController.message_create_get);
+router.post('/create-message', messageController.message_create_post);
+
+// Delete message (Admin only)
+router.post('/delete-message', messageController.message_delete_post);
 
 export default router;
